3827bbd98a9ddddca9b7dcc87bc466f10395b592
Laurent Bachelier <laurent@bachelier.name>
2012-12-22 21:25:20 +0100

    Better shell output, prepare for more outputs

--- cmds.py
+++ cmds.py
@@ -4,21 +4,22 @@ from brutha.tree import Tree
 import argparse
 
 
-def printall(stuff):
-    if isinstance(stuff, basestring):
-        print stuff
-    else:
-        for thing in stuff:
-            printall(thing)
+def sh(commands):
+    print '#!/bin/sh'
+    print 'set +xeu'
+    print "\n\n".join(["\n".join(subcommands) for subcommands in commands])
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-q', '--quality', default=8, type=int)
     parser.add_argument('-g', '--gain', action='store_true')
     parser.add_argument('-d', '--delete', action='store_true')
+    parser.add_argument('-o', '--output', default='sh')
     parser.add_argument('src')
     parser.add_argument('dest')
     args = parser.parse_args()
-    d = Tree(args.src, args.dest,
-             {'quality': args.quality, 'gain': args.gain, 'delete': args.delete})
-    printall(d.commands())
+    tree = Tree(args.src, args.dest,
+                {'quality': args.quality, 'gain': args.gain, 'delete': args.delete})
+    printers = {'sh': sh}
+    printers[args.output](tree.commands())
